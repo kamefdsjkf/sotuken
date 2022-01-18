41436e29775f004a81ca60ee6ad351788f5989f0
unknown <kame.tomin@ezweb.ne.jp>
2022-01-18 14:49:56 +0900

    例外エラー発生中

---
+++ MisaMinoNET-master/MisaMinoNET/MisaMino/stuff.cpp
@@ -0,0 +1,5 @@
+#include "stuff.h"
+
+bool TSD_only = false;
+int sw_map_multiplier = 100;
+bool srs_plus = false; // also disabled LL/RR instructions and enables TETRIO multiplier code
---
+++ MisaMinoNET-master/MisaMinoNET/MisaMino/stuff.h
@@ -0,0 +1,8 @@
+#ifndef STUFF_H
+#define STUFF_H
+
+extern bool TSD_only;
+extern int sw_map_multiplier;
+extern bool srs_plus;
+
+#endif
